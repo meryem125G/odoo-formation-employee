eb84e7bdd6a9dda277e29fcaff090a99bb2d6f33
meryem125G <meryem1jorf@gmail.com>
2025-09-20 21:06:40 +0200

    Description HTML inline pour Odoo Apps

--- FormationEmploye/__manifest__.py
+++ FormationEmploye/__manifest__.py
@@ -5,20 +5,8 @@
     'category': "Human Resources",
     'summary': "Gestion complète des formations des employés",
 
-    'description': """
-<ul>
-    <li>Planification des sessions de formation</li>
-    <li>Suivi administratif des participants</li>
-    <li>Suivi des compétences et indicateurs (KPI)</li>
-    <li>Gestion des formateurs et organismes</li>
-    <li>Organisation par thèmes et modules</li>
-    <li>Analyse et reporting</li>
-</ul>
-<p>Vidéo de démonstration :</p>
-<!--
-<iframe width="560" height="315" src="https://www.youtube.com/embed/TON_ID_VIDEO" frameborder="0" allowfullscreen></iframe>
--->
-""",
+    'description': "<ul><li>Planification des sessions de formation</li><li>Suivi administratif des participants</li><li>Suivi des compétences et indicateurs (KPI)</li><li>Gestion des formateurs et organismes</li><li>Organisation par thèmes et modules</li><li>Analyse et reporting</li></ul><p>Vidéo de démonstration (commentée pour l'instant)</p>",
+
 
 
     'author': "Meravox",
